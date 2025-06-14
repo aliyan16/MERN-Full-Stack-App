9fbe9798a09db7cdb4eb498b1a968d3f9c842296
aliyan16 <aliyanm12376@gmail.com>
2025-06-15 00:02:51 +0500

    implemented routing

--- myfacebook/src/App.js
+++ myfacebook/src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import ProfilePage from "./pages/profilepage";
+import SigninPage from "./pages/Signin";
+import RegistrationPage from "./pages/Registraion";
+import SettingsPage from "./pages/settings";
+import Header from "./components/Header";
 function App() {
   return (
     <>
     <Router>
+      <Header/>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<SigninPage/>} />
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/registration" element={<RegistrationPage/>} />
         <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/setting" element={<SettingsPage/>} />
       </Routes>
     </Router>
     </>
---
+++ myfacebook/src/pages/Signin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function SigninPage(){
+    return(
+        <>
+        SignIn Page
+        </>
+    )
+}
+
+export default SigninPage
